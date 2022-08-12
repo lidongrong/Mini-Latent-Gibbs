a6ff8b4ea5a20ff5101adec6847fa74e64acfda7
lidongrong <32429197+lidongrong@users.noreply.github.com>
2022-08-12 17:37:48 +0800

    Update probit_Main.py

--- Probit/probit_Main.py
+++ Probit/probit_Main.py
@@ -13,13 +13,20 @@ import time
 import os
 from datetime import datetime
 
-beta=np.array([-2,1,4])
-size=1000
-batch_size=100
-n=40000
+beta=np.array([-2,1,3])
+size=2000
+batch_size=125
+n=60000
+rho=0.08
 
 if __name__ == '__main__':
     x,y,z=data_generator(beta,size)
     beta0,z0=initialize(x,y)
     #post_beta,post_z=probit_Gibbs(x,y,z0,beta0,2000)
-    post_beta,post_copy,post_z=batch_probit_Gibbs(x,y,z0,beta0,batch_size,n)
+    #post_beta,post_copy,post_z=batch_probit_Gibbs(x,y,z0,beta0,batch_size,n)
+    our_beta,our_copy,our_z,tau=minibatch_probit_Gibbs(x, y, z0, beta0, batch_size, rho, n)
+    
+    # true
+    dim=beta.shape[0]
+    cov=np.linalg.inv(np.dot(x.T,x)+np.eye(dim))
+    est=np.dot(cov,np.dot(x.T,z))
